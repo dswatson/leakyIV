960535afa649fc44850ed23483e7bbbab8b6edeb
jpenn2023 <149708903+jpenn2023@users.noreply.github.com>
2023-11-02 21:37:55 +0000

    Create jordan_finite_data.R

---
+++ jordan_finite_data.R
@@ -0,0 +1,6 @@
+# Attempt at finite data leaky_IV model with certain confidence bounds 
+# (up to deletion of any uninformative/weakly informative eigen-instruments)
+
+get_bounded_theta <- function(X, Y, Z, ){
+
+}
