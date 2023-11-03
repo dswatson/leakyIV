d96f39792f98171a60c7f718b279c9cb150f05c6
jpenn2023 <149708903+jpenn2023@users.noreply.github.com>
2023-11-03 12:03:22 +0000

    Update jordan_finite_data.R

--- jordan_finite_data.R
+++ jordan_finite_data.R
@@ -1,22 +1,40 @@
 # Attempt at finite data leaky_IV model with certain confidence bounds 
 # (up to deletion of any uninformative/weakly informative eigen-instruments)
+# Assumes different samples are stored as different rows
+
+library(comprehenr)
 
 get_bounded_theta <- function(x, y, z, N, confidence_level){
-
-  x_bounds <-
-  x_sq_bounds <- 
-
-  y_bounds <- 
-  y_sq_bounds <- 
-
+  
+  N <- length(x)
+  
+  x_bounds <- get_bounded_expectation(x, N)
+  x_sq_bounds <- get_bounded_expectation(x*x, N)
+  
+  y_bounds <- get_bounded_expectation(y, N)
+  y_sq_bounds <- get_bounded_expectation(y*y, N)
+  
+  xy_bounds <- get_bounded_expectation(x*y, N)
+  
+  cov_z_lbounds <- matrix(0, N, N)
+  cov_z_ubounds <- matrix(0, N, N)
+  
+  zi_lbounds <- vector(for (i in 1:N) get_bounded_expectation(z[ ,i]*z[ ,j])[1])
+  zi_ubounds <- vector(for (i in 1:N) get_bounded_expectation(z[ ,i]*z[ ,j])[2])
+  
   # For each z_i, z_j find expectation values relevant in cov(z_i, z_j)
-  cov_z_bounded 
+  
+  zi_zj_lbounds <- matrix(for (i in 1:N) for (j in 1:N) get_bounded_expectation(z[ ,i]*z[ ,j])[1], N)
+  zi_zj_ubounds <- matrix(for (i in 1:N) for (j in 1:N) get_bounded_expectation(z[ ,i]*z[ ,j])[2], N)
+  
+  
+  
   
   #FACTORIALLY HARD in number of instruments?? Want bounds in eigenvalues and eigenvectors on cov_z -> 
   #Need to grid search through all allowed cov values?
-  #Could be cheap and sample random allowed cov matrices (maybe always including extreme points) and update bounds on e_vals, e_vecs until bored?
+  #Could be cheap to sample random allowed cov matrices (maybe always including extreme points) and update bounds on e_vals, e_vecs until bored?
   
-  inv_cov_z_bounded
+  
 
   }
 
