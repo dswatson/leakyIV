de7070c3a448c2f94f66279a1f0d96c6e0e6e4e2
David Watson <david.s.watson11@gmail.com>
2024-02-01 09:38:01 +0000

    abc's don't depend on rho

--- derivatives.R
+++ derivatives.R
@@ -3,9 +3,6 @@ library(Deriv)
 
 # Define functions
 norm_mn <- function(rho) {
-  aa <- Sigma_xz %*% Theta_z %*% Sigma_zx - var_x
-  bb <- Sigma_xz %*% Theta_z %*% Sigma_zy - sigma_xy
-  cc <- Sigma_yz %*% Theta_z %*% Sigma_zy - var_y
   theta_r <- bb / aa - sign(rho) * sqrt((bb^2 - aa * cc) * (1 + aa / (eta_x^2 * rho^2))) / 
     (aa * (1 + aa / (eta_x^2 * rho^2)))
   gamma_r <- Theta_z %*% (Sigma_zy - theta_r * Sigma_zx)
@@ -15,9 +12,6 @@ Deriv(norm_mn)
 
 # And the corresponding loss
 loss_fn <- function(rho) {
-  aa <- Sigma_xz %*% Theta_z %*% Sigma_zx - var_x
-  bb <- Sigma_xz %*% Theta_z %*% Sigma_zy - sigma_xy
-  cc <- Sigma_yz %*% Theta_z %*% Sigma_zy - var_y
   theta_r <- bb / aa - sign(rho) * sqrt((bb^2 - aa * cc) * (1 + aa / (eta_x^2 * rho^2))) / 
     (aa * (1 + aa / (eta_x^2 * rho^2)))
   gamma_r <- Theta_z %*% (Sigma_zy - theta_r * Sigma_zx)
