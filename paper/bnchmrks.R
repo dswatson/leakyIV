7ba98cc9a4cda26c9c65ff5f55c7b6ba0263231f
David Watson <david.s.watson11@gmail.com>
2024-02-09 16:15:09 +0000

    midstream

--- paper/bnchmrks.R
+++ paper/bnchmrks.R
@@ -6,6 +6,8 @@ library(data.table)
 library(broom)
 library(sisVIVE)
 library(leakyIV)
+library(ggplot2)
+library(ggsci)
 library(doMC)
 registerDoMC(8)
 
@@ -22,16 +24,12 @@ set.seed(123, kind = "L'Ecuyer-CMRG")
 bnchmrk <- function(z_rho, rho, pr_valid, n = 1000, n_sim = 100) {
   
   # Generate data, extract "population" data
-  if (z_rho != 0) {
-    z_rho <- z_rho * sample(c(1, -1), 1)
-  }
-  rho <- rho * sample(c(1, -1), 1)
   sim <- sim_dat(n = 1e5, d_z = 20, z_cnt = TRUE, z_rho, rho, 
                  theta = 1, r2_x = 3/4, r2_y = 3/4, pr_valid)
   d_z <- 20
   d <- d_z + 2
   l2 <- sqrt(sum(sim$params$gamma^2))
-  tau <- 1.25 * l2
+  tau <- 1.5 * l2
   
   # Inner loop
   inner_loop <- function(b) {
@@ -52,27 +50,27 @@ bnchmrk <- function(z_rho, rho, pr_valid, n = 1000, n_sim = 100) {
     # Run MBE
     beta_hat <- as.numeric(tidy(f0)$estimate[2:(d_z + 1)])
     se_beta <- as.numeric(tidy(f0)$std.error[2:(d_z + 1)])
-    f2 <- lm(y ~ ., data = tmp[, -c('x_hat')])
+    f2 <- lm(y ~ ., data = tmp[, -c('x_hat', 'x')])
     gamma_hat <- tidy(f2)$estimate[2:(d_z + 1)]
     se_gamma <- tidy(f2)$std.error[2:(d_z + 1)]
     mbe <- MBE(beta_hat, gamma_hat, se_beta, se_gamma, phi = 1, n_boot = 1)
     ate_mbe <- mbe$Estimate[2]
     
-    # Bayesian baseline
-    bb_res <- baseline_gaussian_sample(
-      m = 2000, dat = as.matrix(tmp), pos_z = 1:d_z, pos_x = d_z + 1, 
-      pos_y = d_z + 1, prop_var = 0.01, lvar_z_mu = -1, lvar_z_var = 3,
-      beta_var = 5, pre_gamma_var = 1, theta_var = 5, lvar_error_mu = -1,
-      lvar_error_var = 1, tau = tau, alpha = 0.05
-    )
-    ate_bb <- mean(bb_res$theta)
-    ate_lo_bb <- bb_res$lb_interval[2]
-    ate_hi_bb <- bb_res$ub_interval[1]
+    # # Bayesian baseline
+    # bb_res <- baseline_gaussian_sample(
+    #   m = 2000, dat = as.matrix(tmp), pos_z = 1:d_z, pos_x = d_z + 1, 
+    #   pos_y = d_z + 1, prop_var = 0.01, lvar_z_mu = -1, lvar_z_var = 3,
+    #   beta_var = 5, pre_gamma_var = 1, theta_var = 5, lvar_error_mu = -1,
+    #   lvar_error_var = 1, tau = tau, alpha = 0.05
+    # )
+    # ate_bb <- mean(bb_res$theta)
+    # ate_lo_bb <- bb_res$lb_interval[2]
+    # ate_hi_bb <- bb_res$ub_interval[1]
     
     # LeakyIV
     suppressWarnings(
       ate_bnds <- leakyIV(tmp$x, tmp$y, tmp[, -c('x', 'y')], tau = tau,
-                          method = 'glasso', rho = 0.005)
+                          method = 'glasso', rho = 0.01)
     )
     ate_lo <- ate_bnds$ATE_lo
     ate_hi <- ate_bnds$ATE_hi
@@ -97,6 +95,57 @@ df <- foreach(zz = c(0, 0.5), .combine = rbind) %:%
   foreach(rr = c(1/4, 3/4), .combine = rbind) %:%
   foreach(pp = seq(0, 0.95, by = 0.05), .combine = rbind) %dopar%
   bnchmrk(zz, rr, pp)
+
+################################################################################
+
+# Plot it
+df[, mu := mean(theta, na.rm = TRUE), by = .(method, z_rho, rho, pr_valid)]
+df[, se := sd(theta, na.rm = TRUE), by = .(method, z_rho, rho, pr_valid)]
+tmp <- unique(df[, .(method, mu, se, z_rho, rho, pr_valid)])
+tmp[, z_rho := fifelse(z_rho == 0, 'Diagonal', 'Toeplitz')]
+tmp[, rho := fifelse(rho == 0.75, 'Strong Confounding', 'Weak Confounding')]
+#tmp[method == 'TSLS', method := '2SLS']
+#tmp[, method := factor(method, levels = c(''))]
+
+p1 <- ggplot(tmp, aes(pr_valid, mu, color = method, fill = method)) + 
+  geom_ribbon(aes(ymin = mu + se, ymax = mu - se), alpha = 0.4) + 
+  geom_line() + 
+  geom_hline(yintercept = 1, linewidth = 1, color = 'grey60') +
+  scale_color_d3() +
+  scale_fill_d3() +
+  labs(x = 'Proportion of Valid Instruments',
+       y = expression(paste('Average Treatment Effect ', theta))) +
+  facet_grid(z_rho ~ rho) +
+  theme_bw() + 
+  theme(legend.position = 'bottom')
+
+
+
+theme(axis.title = element_text(size = 24),
+      legend.title = element_text(size = 24),
+      legend.text = element_text(size = 24),
+      legend.position = 'bottom')
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
